118b54e5a11e03483916a65e4f121686628b2a29
Alan Luna <za18011658@zapopan.tecmm.edu.mx>
2022-04-21 21:32:08 -0500

    Alan Luna

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ tiempo_estancia_min = 15
 tiempo_estancia_max = 120
 t_llegadas = 20
 t_simulacion = 120
-total_clientes = 5
+total_clientes_aceptan = 54
+total_clientes_no_aceptan = 120
 
 te = 0.0
 dt = 0.0
