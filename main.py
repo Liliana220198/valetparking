88afbfcfc69ad3035267581c373b40d528a55611
Alan Luna <za18011658@zapopan.tecmm.edu.mx>
2022-04-21 21:25:47 -0500

    Alan Luna

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # VALET PARKING...
 
+import random
+import math
+import simpy
+
+seed = 5
+num_autos = 6
+tiempo_estancia_min = 15
+tiempo_estancia_max = 120
+t_llegadas = 20
+t_simulacion = 120
+total_clientes = 5
+
+te = 0.0
+dt = 0.0
+fin = 0.0
+
